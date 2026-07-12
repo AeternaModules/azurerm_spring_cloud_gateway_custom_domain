b309040c7d09aa2695a657c7dfa4e408a0d9f73a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:31:50 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "spring_cloud_gateway_custom_domains_id" {
+  description = "Map of id values across all spring_cloud_gateway_custom_domains, keyed the same as var.spring_cloud_gateway_custom_domains"
+  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.id }
+}
 output "spring_cloud_gateway_custom_domains_name" {
   description = "Map of name values across all spring_cloud_gateway_custom_domains, keyed the same as var.spring_cloud_gateway_custom_domains"
   value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.name }
