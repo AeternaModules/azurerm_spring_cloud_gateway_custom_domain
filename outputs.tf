636bdca3fb34decef48b0e85f2d3097a047216e4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:38 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "spring_cloud_gateway_custom_domains" {
-  description = "All spring_cloud_gateway_custom_domain resources"
-  value       = azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains
-}
 output "spring_cloud_gateway_custom_domains_name" {
-  description = "List of name values across all spring_cloud_gateway_custom_domains"
-  value       = [for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : v.name]
+  description = "Map of name values across all spring_cloud_gateway_custom_domains, keyed the same as var.spring_cloud_gateway_custom_domains"
+  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.name }
 }
 output "spring_cloud_gateway_custom_domains_spring_cloud_gateway_id" {
-  description = "List of spring_cloud_gateway_id values across all spring_cloud_gateway_custom_domains"
-  value       = [for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : v.spring_cloud_gateway_id]
+  description = "Map of spring_cloud_gateway_id values across all spring_cloud_gateway_custom_domains, keyed the same as var.spring_cloud_gateway_custom_domains"
+  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.spring_cloud_gateway_id }
 }
 output "spring_cloud_gateway_custom_domains_thumbprint" {
-  description = "List of thumbprint values across all spring_cloud_gateway_custom_domains"
-  value       = [for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : v.thumbprint]
+  description = "Map of thumbprint values across all spring_cloud_gateway_custom_domains, keyed the same as var.spring_cloud_gateway_custom_domains"
+  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.thumbprint }
 }
 
