65f9473c30b9a31fbc4d2aef4d43ba8cc2025db1
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:09 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "spring_cloud_gateway_custom_domains_id" {
   description = "Map of id values across all spring_cloud_gateway_custom_domains, keyed the same as var.spring_cloud_gateway_custom_domains"
-  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.id }
+  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "spring_cloud_gateway_custom_domains_name" {
   description = "Map of name values across all spring_cloud_gateway_custom_domains, keyed the same as var.spring_cloud_gateway_custom_domains"
-  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.name }
+  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "spring_cloud_gateway_custom_domains_spring_cloud_gateway_id" {
   description = "Map of spring_cloud_gateway_id values across all spring_cloud_gateway_custom_domains, keyed the same as var.spring_cloud_gateway_custom_domains"
-  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.spring_cloud_gateway_id }
+  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.spring_cloud_gateway_id if v.spring_cloud_gateway_id != null && length(v.spring_cloud_gateway_id) > 0 }
 }
 output "spring_cloud_gateway_custom_domains_thumbprint" {
   description = "Map of thumbprint values across all spring_cloud_gateway_custom_domains, keyed the same as var.spring_cloud_gateway_custom_domains"
-  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.thumbprint }
+  value       = { for k, v in azurerm_spring_cloud_gateway_custom_domain.spring_cloud_gateway_custom_domains : k => v.thumbprint if v.thumbprint != null && length(v.thumbprint) > 0 }
 }
 
